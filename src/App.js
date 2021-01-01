9deddfc05b202f0f97a347773d18e26ad69f679a
Ryan <44737251+rsuttle@users.noreply.github.com>
2020-12-31 23:24:07 -0500

    Coordinates are now converted to (long,lat) and geoJson properly generates

--- src/App.js
+++ src/App.js
@@ -11,29 +11,47 @@ import {
 } from "react-router-dom";
 import MapComponent from "./components/Map"
 import testImage from "./images/testradar.PNG";
-import blurredTestImage from "./images/croppedTestRadar1.png"
+import blurredTestImage from "./images/testradar.PNG"
 import { clearStorage } from 'mapbox-gl';
+import { act } from 'react-dom/test-utils';
 var Color = require("color");
 const proj4 = require("proj4");
 
+//Converts an array of planar coordinates to their latitude/longitude equivalent.
+const convertCoordinatesToLatLong = (oldCoordinates, minLng, maxLng, minLat, maxLat, width, height) => {
+
+  let newCoordinates = [];
+
+  //The GeoJSON data that you feed into Tippecanoe should be in EPSG:4326
+  oldCoordinates.forEach((oldCoord) => {
+
+    // let newCoord = proj4('EPSG:3857', 'EPSG:4326', [
+    //   minLng + (maxLng - minLng) * (oldCoord[0] / width),
+    //   maxLat - (maxLat - minLat) * (oldCoord[1] / height)
+    // ]);
+
+    let newCoord = [minLng + (maxLng - minLng) * (oldCoord[0] / width),
+    maxLat - (maxLat - minLat) * (oldCoord[1] / height)];
+
+    //let newCoord = [maxLat - (maxLat - minLat) * (oldCoord[1] / height),minLng + (maxLng - minLng) * (oldCoord[0] / width)];
+
+
+
+    newCoordinates.push(newCoord);
+  });
+
+  return newCoordinates;
+
+}
+
 
 const convertPixelstoGeoJson = (pixelVals) => {
-  let w = 154;
-  let h = 137;
+  let w = 600;
+  let h = 629;
 
-  var thresholdArray = [];
-  for (let i = 0; i <= 9; i += 0.2) {
-    thresholdArray.push(i);
-  }
+
   //creates geoJSON contour generator, results in geojson with bounds of inputted size
-  var polygons = contours().size([154, 137]).smooth(false).thresholds([0.2,1.2,2.2,3.2,4.2,5.2])
-
-  //var density = contourDensity().size([600, 629]).thresholds([0,1,2,3,4,5,6,7,8,9]);
-
-
-
-
-  console.log("creating geojson");
+  var polygons = contours().size([600, 629]).smooth(true).thresholds([0.2, 1.2, 2.2, 3.2, 4.2, 5.2])
 
   let pls = polygons(pixelVals);
 
@@ -46,28 +64,56 @@ const convertPixelstoGeoJson = (pixelVals) => {
 
 
   pls.forEach((multipolygon) => {
-    
+
 
     multipolygon.coordinates.forEach((actualPolygon) => {
-      resultgeojson.features.push({
-        type: 'Feature',
-        properties: {
-          value: multipolygon.value,
-          idx: 0
-        },
-        geometry: {
-          type: 'Polygon',
-          coordinates: actualPolygon
-        }
-      });
-    })
+
+      //some of them are 2+d arrays
+
+      if (actualPolygon.length > 1) {
+        console.log("weird array detected: ", actualPolygon);
+        actualPolygon.forEach((arr) => {
+          let convertedArr = [convertCoordinatesToLatLong(arr,-96, -78, 31, 48, w, h)];
+          //let convertedArr = [arr];
+          resultgeojson.features.push({
+            type: 'Feature',
+            properties: {
+              value: multipolygon.value,
+              idx: 0
+            },
+            geometry: {
+              type: 'Polygon',
+              coordinates: convertedArr
+            }
+          });
+
+        });
+      }
+      else {
+        let convertedArr = [convertCoordinatesToLatLong(actualPolygon[0], -96, -78, 31, 48, w, h)];
+        //let convertedArr = actualPolygon;
+        resultgeojson.features.push({
+          type: 'Feature',
+          properties: {
+            value: multipolygon.value,
+            idx: 0
+          },
+          geometry: {
+            type: 'Polygon',
+            coordinates: convertedArr
+          }
+        });
+      }
+
+    });
 
 
   });
 
+  console.log("geojson:");
   console.log(JSON.stringify(resultgeojson));
 
-  
+
 
 
 
@@ -77,10 +123,15 @@ const convertRGBAToPixels = (rgbaArray) => {
 
 
   var whiteBackground = Color.rgb([255, 255, 255]);
+
   var lightBlue = Color.rgb([102, 243, 244]);
+
   var medBlue = Color.rgb([125, 196, 250]);
+
   var darkBlue = Color.rgb([134, 102, 249]);
+
   var lightGreen = Color.rgb([102, 255, 115]);
+
   var medGreen = Color.rgb([102, 223, 111]);
   var darkGreen = Color.rgb([102, 189, 107]);
   var lightYellow = Color.rgb([253, 255, 113]);
@@ -140,23 +191,25 @@ function App() {
 
     var imgCanvas = canvasRef.current;
     var ctx = imgCanvas.getContext("2d");
-    imgCanvas.width = 154;
-    imgCanvas.height = 137;
+    imgCanvas.width = 600;
+    imgCanvas.height = 629;
 
 
     var radarImg = new Image();
 
     radarImg.addEventListener('load', function () {
-      
-      ctx.setTransform(1,0,0,-1,0,imgCanvas.height);
+
+      //flipping across x-axis
+      //ctx.setTransform(1, 0, 0, -1, 0, imgCanvas.height);
       ctx.drawImage(radarImg, 0, 0);
 
-      var myImageData = ctx.getImageData(0, 0, 154, 137);
+      var myImageData = ctx.getImageData(0, 0, 600, 629);
 
-      ctx.setTransform(1,0,0,1,0,0);
-      ctx.drawImage(radarImg, 0, 0);
+      //flipping it back for viewing
+      // ctx.setTransform(1, 0, 0, 1, 0, 0);
+      // ctx.drawImage(radarImg, 0, 0);
 
-      console.log(myImageData);
+
 
 
       var imageConvertedToValues = convertRGBAToPixels(myImageData.data);
@@ -169,12 +222,7 @@ function App() {
 
 
 
-    //The GeoJSON data that you feed into Tippecanoe should be in EPSG:4326
-    // let pixel_position = [10, 20];
-    // let geo_position = proj4('EPSG:3857', 'EPSG:4326',[
-    //     minLng + (maxLng - minLng) * (p[0] / w),
-    //     maxLat - (maxLat - minLat) * (p[1] / h)
-    // ]);
+
   }, [])
 
   return (
