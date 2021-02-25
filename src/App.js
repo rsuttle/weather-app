bfe88898068b3e273e5a82dd3ede6418f8bad721
Ryan <44737251+rsuttle@users.noreply.github.com>
2021-02-24 21:53:21 -0500

    Made the  map full screen, and set up viewport to start zoomed in at the center of the map

--- src/App.js
+++ src/App.js
@@ -80,52 +80,11 @@ function App() {
   
   return (
     <>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/map">Map</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>
-
-
-          <Switch>
-            <Route path="/map">
-              <Map/>
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <Map/>
       
       
     </>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -6,6 +6,8 @@ import { Viewport } from "pixi-viewport";
 import axios from "axios";
 
 var usMap = require('../deleteLater/simplifiedUsTopoJson.json');
+var usOutline = require('../deleteLater/usOutline.json');
+
 
 
 
@@ -14,11 +16,13 @@ var usMap = require('../deleteLater/simplifiedUsTopoJson.json');
 
 const Map = () => {
 
-    const width = 975
-    const height = 610
+    const width = 975;
+    const height = 610;
+
+    const userWindowWidth = window.innerWidth;
+    const userWindowHeight = window.innerHeight;
 
     const [radarFrames, setRadarFrames] = useState(null);
-
 
     const renderCanvas = useRef(null);
     const pixiRenderer = useRef(null);
@@ -26,24 +30,22 @@ const Map = () => {
     const pixiTicker = useRef(new PIXI.Ticker());
     const stage = useRef(new PIXI.Container());
     const currentFrame = useRef(0);
-
     const graphics = useRef(new PIXI.Graphics());
     const path = useRef(null);
     const projPath = useRef(null);
 
-
-
-
-
     var projection = d3.geoAlbersUsa().scale(1300).translate([487.5, 305]);
+    //var projection = d3.geoAlbersUsa().scale(1300).translate([userWindowWidth/2, userWindowHeight/2]);
+    //var projection = d3.geoAlbersUsa().scale(1300).translate([1000, 500]);
 
 
 
     useEffect(() => {
 
-        
+
         axios.get('http://192.168.0.5:8000')
             .then(function (response) {
+
                 setRadarFrames(response.data);
                 console.log(response.data);
 
@@ -52,14 +54,14 @@ const Map = () => {
 
 
         //setup renderer with canvas
-        pixiRenderer.current = new PIXI.Renderer({ width: width, height: height, backgroundColor: 0x7d7d7d, antialias: true, view: renderCanvas.current });
+        pixiRenderer.current = new PIXI.Renderer({ width: userWindowWidth, height: userWindowHeight, backgroundColor: 0x7d7d7d, antialias: true, view: renderCanvas.current });
 
         //create viewport and add to stage
         const viewport = new Viewport({
-            screenWidth: width,
-            screenHeight: height,
-            worldWidth: 975,
-            worldHeight: 610,
+            screenWidth: userWindowWidth,
+            screenHeight: userWindowHeight,
+            worldWidth: 2000,
+            worldHeight: 2000,
             interaction: pixiRenderer.current.plugins.interaction
         });
         stage.current.addChild(viewport);
@@ -69,15 +71,16 @@ const Map = () => {
             .pinch()
             .wheel()
             .decelerate()
-            .clamp({ left: 0, right: 975, bottom: 610, top: 0 })
-            .clampZoom({ minScale: 1, maxScale: 10 })
+            .clamp({ left: 480, right: 800, bottom: 350, top: 50 })
+            .clampZoom({ minScale: 4.5, maxScale: 20 })
+            .moveCenter(665,210)
 
 
 
 
         path.current = d3.geoPath(null, graphics.current);
         projPath.current = d3.geoPath(projection, graphics.current);
-       
+
 
         viewport.addChild(graphics.current);
 
@@ -94,35 +97,34 @@ const Map = () => {
 
     }, []);
 
+    //console.log(JSON.stringify(topojson.mesh(usMap, usMap.objects.states, (a, b) => a !== b)));
+    
+   
+    var colorMap = {
+        10: 0x66f4f4,
+        15: 0x7dc4fa,
+        20: 0x8666f9,
+        25: 0x66ff73,
+        30: 0x66df6f,
+        35: 0x66bd6b,
+        40: 0xfdff71,
+        7.8: 0xefdb6b,
+        8.8: 0xfebe66
+    };
+
     useEffect(() => {
-        console.dir(loop);
+        //console.dir(loop);
 
         function loop() {
 
             graphics.current.clear();
-           
-        
 
-            console.log(graphics.current.geometry);
+
+
+           
             graphics.current.lineStyle(1 / pixiViewport.current.transform.scale.x, 0x000000, 1);
-            
-            
-            
-               console.log("running")
-               console.log("space")
-           
 
-            var colorMap = {
-                10: 0x66f4f4,
-                15: 0x7dc4fa,
-                20: 0x8666f9,
-                25: 0x66ff73,
-                30: 0x66df6f,
-                35: 0x66bd6b,
-                40: 0xfdff71,
-                7.8: 0xefdb6b,
-                8.8: 0xfebe66
-            };
+           
 
 
             if (radarFrames !== null) {
@@ -141,8 +143,8 @@ const Map = () => {
 
                 //draw radar frame
                 for (let i = 0; i < why.geometries.length; i++) {
-                    graphics.current.beginFill(colorMap[why.geometries[i].value],0.5);
-                    
+                    graphics.current.beginFill(colorMap[why.geometries[i].value], 0.5);
+
                     path.current(why.geometries[i]);
                     graphics.current.endFill();
 
@@ -150,17 +152,21 @@ const Map = () => {
                 }
             }
 
-           // console.log(graphics.current.scale)
+           
 
+
+
+           //draw states and US outline
+            path.current(usMap);
+            path.current(usOutline);
+            
             
 
-            
-            path.current(topojson.mesh(usMap, usMap.objects.states, (a, b) => a !== b));
-            path.current(topojson.feature(usMap, usMap.objects.nation));
-            //console.log(graphics.current.geometry.data);
+
+
             pixiRenderer.current.render(stage.current);
-            currentFrame.current = (currentFrame.current + 1) %6;
-            
+            currentFrame.current = (currentFrame.current + 1) % 6;
+
 
 
 
@@ -173,9 +179,9 @@ const Map = () => {
         pixiTicker.current.add(loop, PIXI.UPDATE_PRIORITY.LOW);
         pixiTicker.current.start();
 
-        
-        
-        console.log("ticker added")
+
+
+        //console.log("ticker added")
     }, [radarFrames]);
 
 
